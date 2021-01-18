50b56d1869d74b49f5d1e83bc179c447b01b088d
Roman Egorov <indorm1992@mail.ru>
2021-01-18 12:37:36 +0300

    hometask_4 - add linear and relative profile layouts

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -0,0 +1,21 @@
+package ru.skillbranch.devintensive.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.skillbranch.devintensive.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+
+    }
+}
